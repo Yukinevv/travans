78c3a87bbf72f4b7a334a89e79be2033d049a285
Yukinevv <adrianrodzic33@gmail.com>
2026-04-19 18:55:50 +0200

    Rozsuwane menu boczne

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from './modules/auth/services/auth.service';
@@ -9,14 +9,49 @@ import { AuthService } from './modules/auth/services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  sidebarOpen = true;
+  isMobileViewport = false;
+
   constructor(
     public readonly authService: AuthService,
     private readonly router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.updateSidebarForViewport();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.updateSidebarForViewport();
+  }
+
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.sidebarOpen = false;
+  }
+
+  closeSidebarAfterNavigation(): void {
+    if (this.isMobileViewport) {
+      this.sidebarOpen = false;
+    }
+  }
+
   logout(): void {
     this.authService.logout();
+    this.sidebarOpen = !this.isMobileViewport;
     this.router.navigate(['/auth']);
+  }
+
+  private updateSidebarForViewport(): void {
+    const mobileViewport = window.innerWidth <= 960;
+    if (mobileViewport !== this.isMobileViewport) {
+      this.isMobileViewport = mobileViewport;
+      this.sidebarOpen = !mobileViewport;
+    }
   }
 }
